bd8406a34020b89682b00cf25c1282f84596df5a
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-11-14 01:25:03 +0100

    Use python-decouple to manage DEBUG setting in settings.py

--- timerPlayingRoom/timerPlayingRoom/settings.py
+++ timerPlayingRoom/timerPlayingRoom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+&6jh52sz1jl0$off^v!dm7fvpg0o+oj3*7phyzweg-uq%i3lu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
