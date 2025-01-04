92e8d7b84168ddd000371fc2b4c4531d90a7e12a
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-04 07:20:31 +0100

    add method to format message

--- timerPlayingRoom/timer/models.py
+++ timerPlayingRoom/timer/models.py
@@ -9,22 +9,24 @@ from django.conf import settings
 
 # Create your models here.
 
+
 class Timer(models.Model):
     launch_time = models.DateTimeField()
 
     def __str__(self):
         return f"Timer ending at {self.launch_time}"
-    
+
     def display_time(self):
-
         return self.launch_time.timestamp() * 1000
-    
-    def is_over(self):
-        time = 45*60*1000
 
-        is_over = timezone.now().timestamp() * 1000 - self.launch_time.timestamp() * 1000 > time
+    def is_over(self):
+        time = 45 * 60 * 1000
+
+        is_over = (
+            timezone.now().timestamp() * 1000 - self.launch_time.timestamp() * 1000
+            > time
+        )
         return is_over
-    
 
     def send_ticket(self, type):
         time = self.display_time()
@@ -43,6 +45,7 @@ class Timer(models.Model):
             },
         )
 
+
 class Message(models.Model):
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
@@ -52,7 +55,6 @@ class Message(models.Model):
         return f"Message at {self.date}"
 
     def send_ticket(self):
-
         channel_layer = channels.layers.get_channel_layer()
         async_to_sync(channel_layer.group_send)(
             settings.TICKS_GROUP_NAME,
@@ -61,9 +63,20 @@ class Message(models.Model):
                 "content": json.dumps(
                     {
                         "type": "message",
-                        "message": self.content,
+                        "message": self.formated_message(),
                     }
                 ),
             },
         )
 
+    def formated_message(self):
+        message = self.content
+        if (
+            not self.content.endswith(".")
+            and not self.content.endswith("!")
+            and not self.content.endswith("?")
+            and not self.content == ""
+        ):
+            message += "."
+        message = message.capitalize()
+        return message
