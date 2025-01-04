fb25da4e468cc5d83b0e445b4fc035761420520a
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-04 04:17:52 +0100

    update settings.py to configure ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS using environment variables

--- timerPlayingRoom/timerPlayingRoom/settings.py
+++ timerPlayingRoom/timerPlayingRoom/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config("BACKEND_HOST", default="127.0.0.1")]
+
+CSRF_TRUSTED_ORIGINS = ["https://" + config("BACKEND_HOST", default="")]
+
 
 # Application definition
 
